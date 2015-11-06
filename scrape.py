17445b200d6544bd6a4ef435ab5432d351ec1d26
charu bansal <charubansal102@gmail.com>
2015-11-06 19:03:48 +0530

    Create scrape.py

---
+++ scrape.py
@@ -0,0 +1,18 @@
+import requests
+import urllib
+import BeautifulSoup
+import os
+
+#Outputs the entire html source code and prints it
+url = 'http://www-rohan.sdsu.edu/~gawron/python_for_ss/course_core/book_draft/web/HTMLParser.html'
+response = requests.get(url)
+html = response.content
+print html
+
+raw_input("Press enter to jumo to next")
+os.system('clear')
+thisurl = 'http://www-rohan.sdsu.edu/~gawron/python_for_ss/course_core/book_draft/web/HTMLParser.html'
+handle = urllib.urlopen(thisurl)
+html_gunk = handle.read()
+#print html_gunk
+print html_gunk[:150]    #prints the first 150 letter
